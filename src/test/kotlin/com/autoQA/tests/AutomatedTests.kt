9d70c07781c4c8c2e637a749129e90c44980e962
MRGuziX <tomasz.guzik@onet.eu>
2022-12-19 12:00:01 +0100

    [POM_featureNumber]: - uncommented tests - added LoginPage and BasePage

--- src/test/kotlin/com/autoQA/tests/AutomatedTests.kt
+++ src/test/kotlin/com/autoQA/tests/AutomatedTests.kt
@@ -1,24 +1,29 @@
 package com.autoQA.tests
 
 import com.autoQA.utilities.DriverFunctions
+import com.autoQA.utilities.DriverFunctions.Companion.driver
 import org.testng.Assert
+import org.testng.annotations.AfterTest
 import org.testng.annotations.BeforeTest
 import org.testng.annotations.Test
+import pages.BasePage
 import pages.LoginPage
 
-class AutomatedTests: DriverFunctions() {
+class AutomatedTests {
 
     private val loginPage = LoginPage()
+    private val basePage = BasePage()
+    private val driverFunction = DriverFunctions()
 
     @BeforeTest
     fun createDriver() {
-        buildDriver()
+        driverFunction.buildDriver()
     }
 
     @Test(priority = 0, groups = ["Smoke Tests"])
     fun openWebPage() {
         val url = "https://www.saucedemo.com/"
-        openURL(url)
+        driverFunction.openURL(url)
 
         Assert.assertEquals(driver.currentUrl, url, "Web page URL did not match!")
     }
@@ -30,90 +35,85 @@ class AutomatedTests: DriverFunctions() {
         loginPage.enterPassword()
         loginPage.clickOnLoginButton()
     }
-//
-//    @Test(priority = 2, groups = ["Smoke Tests"])
-//    fun addItemsToCart() {
-//
-//        clickOnElementByID("add-to-cart-sauce-labs-bolt-t-shirt")
-//        clickOnElementByID("add-to-cart-sauce-labs-fleece-jacket")
-//
-//        val itemName = getElementTextByCSS("#item_1_title_link .inventory_item_name")
-//        val itemPrice = getElementTextByXPATH("//a[@id='item_1_title_link']/..//following-sibling::*[@class='pricebar']/div")
-//        val itemCounter = getElementTextByCSS(".shopping_cart_badge")
-//
-//        Assert.assertEquals(itemCounter,"2","Item counts do not match")
-//
-//        clickOnElementByID("shopping_cart_container")
-//
-//        val itemNameInCartElement = getElementsTextByClassName("inventory_item_name",0)
-//        val itemPriceInCartElement = getElementsTextByClassName("inventory_item_price",0)
-//
-//        Assert.assertEquals(itemName,itemNameInCartElement,"Names do not match!")
-//        Assert.assertEquals(itemPrice,itemPriceInCartElement,"Prices do not match!")
-//
-//        clickOnElementByID("continue-shopping")
-//    }
-//
-//    @Test(priority = 3, groups = ["Smoke Tests"])
-//    fun removeItemFromCart() {
-//
-//        clickOnElementByID("remove-sauce-labs-bolt-t-shirt")
-//        clickOnElementByID("add-to-cart-sauce-labs-bolt-t-shirt")
-//        clickOnElementByID("shopping_cart_container")
-//        clickOnElementByID("remove-sauce-labs-bolt-t-shirt")
-//
-//        val itemCounter = getElementTextByCSS(".shopping_cart_badge")
-//
-//        Assert.assertEquals(itemCounter,"1","Item counts do not match")
-//
-//        clickOnElementByID("continue-shopping")
-//    }
-//
-//    @Test(priority = 4, groups = ["Smoke Tests"])
-//    fun sortingNameFromZtoA () {
-//
-//        val nameOfTheFirstElementBeforeSort = getElementsTextByClassName("inventory_item_name",0)
-//        selectFromDropDown("product_sort_container","Name (Z to A)")
-//        val nameOfTheFirstElementAfterSort = getElementsTextByClassName("inventory_item_name",0)
-//
-//        Assert.assertNotEquals(nameOfTheFirstElementAfterSort,nameOfTheFirstElementBeforeSort,"First product before and after are the same! Sorting Broken!")
-//        }
-//
-//    @Test(priority = 5, groups = ["Smoke Tests"])
-//    fun sortingPriceFromLowestToHighest () {
-//
-//        selectFromDropDown("product_sort_container","Price (low to high)")
-//        val nameOfTheFirstElementBeforeSort = getElementsTextByClassName("inventory_item_name",0)
-//        selectFromDropDown("product_sort_container","Price (high to low)")
-//        val nameOfTheFirstElementAfterSort = getElementsTextByClassName("inventory_item_name",0)
-//
-//        Assert.assertNotEquals(nameOfTheFirstElementAfterSort,nameOfTheFirstElementBeforeSort,"First product cost before and after are the same! Sorting Broken!")
-//    }
-//
-//    @Test(priority = 6, groups = ["Smoke Tests"])
-//    fun checkoutFormFill() {
-//
-//        clickOnElementByID("shopping_cart_container")
-//        clickOnElementByID("checkout")
-//        enterDataByID("first-name","First Name")
-//        enterDataByID("last-name","Last Name")
-//        enterDataByID("postal-code","12-123")
-//        clickOnElementByID("continue")
-//        clickOnElementByID("finish")
-//
-//        val finishOrderTextElement = getElementTextByClassName("complete-header")
-//        Assert.assertEquals(finishOrderTextElement,"THANK YOU FOR YOUR ORDER", "Finish Order text it is not matching!")
-//    }
-//
-//    @Test(priority = 7, groups = ["Smoke Tests"])
-//    fun checkOutFormWarnings() {
-//
-//    }
-//
-//    @AfterTest(groups = ["Smoke Tests"])
-//    fun tearDownDriver() {
-//        driver.quit()
-//    }
+
+    @Test(priority = 2, groups = ["Smoke Tests"])
+    fun addItemsToCart() {
+
+        basePage.addProduct("add-to-cart-sauce-labs-bolt-t-shirt")
+        basePage.addProduct("add-to-cart-sauce-labs-fleece-jacket")
+
+        val itemName = driverFunction.getElementTextByCSS("#item_1_title_link .inventory_item_name")
+        val itemPrice = driverFunction.getElementTextByXPATH("//a[@id='item_1_title_link']/..//following-sibling::*[@class='pricebar']/div")
+        val itemCounter = driverFunction.getElementTextByCSS(".shopping_cart_badge")
+
+        Assert.assertEquals(itemCounter,"2","Item counts do not match")
+
+        driverFunction.clickOnElementByID("shopping_cart_container")
+
+        val itemNameInCartElement = driverFunction.getElementsTextByClassName("inventory_item_name",0)
+        val itemPriceInCartElement = driverFunction.getElementsTextByClassName("inventory_item_price",0)
+
+        Assert.assertEquals(itemName,itemNameInCartElement,"Names do not match!")
+        Assert.assertEquals(itemPrice,itemPriceInCartElement,"Prices do not match!")
+
+        driverFunction.clickOnElementByID("continue-shopping")
+    }
+
+    @Test(priority = 3, groups = ["Smoke Tests"])
+    fun removeItemFromCart() {
+
+        driverFunction.clickOnElementByID("remove-sauce-labs-bolt-t-shirt")
+        driverFunction.clickOnElementByID("add-to-cart-sauce-labs-bolt-t-shirt")
+        driverFunction.clickOnElementByID("shopping_cart_container")
+        driverFunction.clickOnElementByID("remove-sauce-labs-bolt-t-shirt")
+
+        val itemCounter = driverFunction.getElementTextByCSS(".shopping_cart_badge")
+
+        Assert.assertEquals(itemCounter,"1","Item counts do not match")
+
+        driverFunction.clickOnElementByID("continue-shopping")
+    }
+
+    @Test(priority = 4, groups = ["Smoke Tests"])
+    fun sortingNameFromZtoA () {
+
+        val nameOfTheFirstElementBeforeSort = driverFunction.getElementsTextByClassName("inventory_item_name",0)
+        driverFunction.selectFromDropDown("product_sort_container","Name (Z to A)")
+        val nameOfTheFirstElementAfterSort = driverFunction.getElementsTextByClassName("inventory_item_name",0)
+
+        Assert.assertNotEquals(nameOfTheFirstElementAfterSort,nameOfTheFirstElementBeforeSort,"First product before and after are the same! Sorting Broken!")
+        }
+
+    @Test(priority = 5, groups = ["Smoke Tests"])
+    fun sortingPriceFromLowestToHighest () {
+
+        driverFunction.selectFromDropDown("product_sort_container","Price (low to high)")
+        val nameOfTheFirstElementBeforeSort = driverFunction.getElementsTextByClassName("inventory_item_name",0)
+        driverFunction.selectFromDropDown("product_sort_container","Price (high to low)")
+        val nameOfTheFirstElementAfterSort = driverFunction.getElementsTextByClassName("inventory_item_name",0)
+
+        Assert.assertNotEquals(nameOfTheFirstElementAfterSort,nameOfTheFirstElementBeforeSort,"First product cost before and after are the same! Sorting Broken!")
+    }
+
+    @Test(priority = 6, groups = ["Smoke Tests"])
+    fun checkoutFormFill() {
+
+        driverFunction.clickOnElementByID("shopping_cart_container")
+        driverFunction.clickOnElementByID("checkout")
+        driverFunction.enterDataByID("first-name","First Name")
+        driverFunction.enterDataByID("last-name","Last Name")
+        driverFunction.enterDataByID("postal-code","12-123")
+        driverFunction.clickOnElementByID("continue")
+        driverFunction.clickOnElementByID("finish")
+
+        val finishOrderTextElement = driverFunction.getElementTextByClassName("complete-header")
+        Assert.assertEquals(finishOrderTextElement,"THANK YOU FOR YOUR ORDER", "Finish Order text it is not matching!")
+    }
+
+    @AfterTest(groups = ["Smoke Tests"])
+    fun tearDownDriver() {
+        driver.quit()
+    }
 
 }
 
